29e20e066ba62f6b402d87cfced8ac4c7a0d8344
solonirinafalda335-star <solonirinafalda335@gmail.com>
2025-08-02 14:27:25 +0300

    Initial commit NexTrade Server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const fs = require('fs');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
